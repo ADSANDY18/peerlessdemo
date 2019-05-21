4dc2f00e48d1f5c4f79ca634ccbc892dd1f1f1ac
ADSANDY18 <49060791+ADSANDY18@users.noreply.github.com>
2019-05-21 14:38:07 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+   <script type="text/javascript">
+    /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+    (function(d, m){
+      var kommunicateSettings = {"appId":"c5d9704253e2ae90bfc79e4875dc1dd5","conversationTitle":"Sandip Ghosh","automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+</script>
+    
+    
+    </head>
+
+
+</html>
